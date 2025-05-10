c4d125b446ed5fffc2c4a38f1a75611de1767eb6
Sandip Nandy <sandipnandy9@gmail.com>
2025-05-10 11:16:39 +0530

    descriptions added in index. js for better understanding

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+//Server code to send emails using SendGrid API for my portfolio website
 import express from "express";
 import sgMail from "@sendgrid/mail";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
-const app = express();
+const app = express(); // Create an Express application
+// Set the port for the server
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors()); //cors is a package that allows you to enable Cross-Origin Resource Sharing (CORS) in your Express application.
+// This is useful when your frontend and backend are running on different domains or ports.
+app.use(express.json()); 
 
 // Set SendGrid API Key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -17,7 +20,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 // Email Route
 app.post("/send-email", async (req, res) => {
   const { to, from, subject, text } = req.body;
-
+  
   const msg = {
     to,
     from,
